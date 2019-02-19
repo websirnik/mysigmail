7437bb0eeb502a7db1957eb3a6b97ba6eb3988cd
Anton Reshetov <reshetov.art@gmail.com>
2019-02-19 17:40:48 +0300

    feat(editor): add email layout component

---
+++ src/components/email/EmailLayout.vue
@@ -0,0 +1,27 @@
+<template>
+  <table
+    cellpadding="0"
+    cellspacing="0"
+    border="0"
+    width="100%"
+    class="email-layout"
+  >
+    <slot />
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'EmailLayout',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
